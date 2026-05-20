a06d3e3f74a82010c8e9e46efb61d19fcf6d5ff8
Artur Twardzik <polym.t@protonmail.com>
2026-05-20 15:24:49 +0200

    fix: change db tables' and columns' names

--- karting/blueprints/eventsManagement.py
+++ karting/blueprints/eventsManagement.py
@@ -24,13 +24,13 @@ def manage_events():
     if request.method == "POST":
         data = request.json
         cur.execute(
-            "INSERT INTO karting_event (name, date, type, track_id) VALUES (%s, %s, %s, %s)",
+            "INSERT INTO events (name, date, type, track_id) VALUES (%s, %s, %s, %s)",
             (data.get("name"), data.get("date"), data.get("type"), data.get("track_id")),
         )
         get_db().connection.commit()
         return jsonify({"message": "Event added", "id": cur.lastrowid}), 201
 
-    cur.execute("SELECT * FROM karting_event")
+    cur.execute("SELECT * FROM events")
     columns = [col[0] for col in cur.description]
     rows = cur.fetchall()
 
@@ -47,7 +47,7 @@ def record_race():
     if "user_id" not in session:
         return jsonify({"error": "Unauthorized: Please log in."}), 401
     cur = get_db()
-    cur.execute("SELECT rola_id FROM users WHERE user_id = %s", (session["user_id"],))
+    cur.execute("SELECT role_id FROM users WHERE user_id = %s", (session["user_id"],))
     user_role = cur.fetchone()
 
     if not user_role or user_role[0] != 1:
@@ -61,7 +61,7 @@ def record_race():
 
     try:
         cur.execute(
-            "INSERT INTO wyscig (warunki_pogodowe, wydarzenie_id) VALUES (%s, %s)", (weather_conditions, event_id)
+            "INSERT INTO races (weather_conditions), event_id) VALUES (%s, %s)", (weather_conditions, event_id)
         )
         race_id = cur.lastrowid
 
@@ -71,14 +71,14 @@ def record_race():
             start_position = part.get("start_position")
             end_position = part.get("end_position")
             cur.execute(
-                """INSERT INTO udzial (pozycja_startowa, pozycja_koncowa, zawodnik_id, gokart_id, wyscig_id)
+                """INSERT INTO participations (starting_position, finishing_position, competitor_id, gokart_id, race_id)
                    VALUES (%s, %s, %s, %s, %s)""",
                 (start_position, end_position, competitor_id, gokart_id, race_id),
             )
 
             if gokart_id:
                 cur.execute(
-                    """UPDATE component
+                    """UPDATE components
                        SET engine_hours = engine_hours + %s, mileage = mileage + %s
                        WHERE gokart_id = %s""",
                     (race_duration, track_length, gokart_id),
